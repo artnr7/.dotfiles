df19909aef7986a36ee883f365f979fbd4bfc943
Nigmatullin Artur <artnr7@gmail.com>
2026-01-12 23:16:31 +0300

    work

--- .config/nvim/lua/plugins/centerpad.lua
+++ .config/nvim/lua/plugins/centerpad.lua
@@ -1,6 +1,13 @@
 return {
   {
     "smithbm2316/centerpad.nvim",
-    config = function() end,
+    config = function()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>z",
+        "<cmd>Centerpad 100 10<cr>",
+        { silent = true, noremap = true }
+      )
+    end,
   },
 }
